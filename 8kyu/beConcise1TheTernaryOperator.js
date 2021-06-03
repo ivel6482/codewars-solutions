fe3bdecbc4eac1701d2007395ffa69fdce638cd5
Ilder Velez <velez.ilder@gmail.com>
2021-06-03 09:21:25 -0400

    Add solution

---
+++ 8kyu/beConcise1TheTernaryOperator.js
@@ -0,0 +1,13 @@
+// TODO: Refactor and shorten the function
+
+function describeAge(age) {
+	return `You're a(n) ${
+		age < 13
+			? 'kid'
+			: age > 12 && age < 18
+			? 'teenager'
+			: age > 17 && age < 65
+			? 'adult'
+			: 'elderly'
+	}`
+}
