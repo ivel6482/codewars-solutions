f0f1c29af5d645ac63b5371049a882ffdecc4bee
Ilder Velez <velez.ilder@gmail.com>
2021-07-13 12:58:16 -0400

    Add solution

---
+++ 8kyu/forUFCFansTotalBeginnersConorMcGregorVsGeorgeSaintPierre.js
@@ -0,0 +1,6 @@
+var quote = function (fighter) {
+	// your code here
+	return fighter.toLowerCase() === 'conor mcgregor'
+		? "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+		: 'I am not impressed by your performance.'
+}
