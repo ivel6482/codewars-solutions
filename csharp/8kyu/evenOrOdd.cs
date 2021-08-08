cc19efa548da1d1424683c4e9e9c5bb65a33ac08
Ilder Velez <velez.ilder@gmail.com>
2021-08-08 00:10:40 -0400

    Add solution

---
+++ csharp/8kyu/evenOrOdd.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Solution
+{
+  public class SolutionClass
+  {
+    public static string EvenOrOdd(int number)
+    {
+      // Code goes here ...
+      return number % 2 == 0 ? "Even" : "Odd";
+    }
+  }
+}
